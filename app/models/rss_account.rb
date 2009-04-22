74f08a09f3e7f05d54617e2be5a6c28fdb4b086f
Dan Nawara <dan@nawara.com>
2009-04-21 20:04:25 -0500

    fixed nil resluts for rss type

--- app/models/rss_account.rb
+++ app/models/rss_account.rb
@@ -6,16 +6,16 @@ class RssAccount < Account
 
         response = http.read
         result = RSS::Parser.parse(response, false)
-
-        result.items.each do |item|
-          @item = Item.new()
-          @item.service = "rss"
-          @item.content = item.description
-          @item.created_at = item.date.to_date
-          @item.user_id = account.user_id
-          @item.save() if Item.find_by_content(item.description).nil?
-        end
-
+unless result.nil?
+          result.items.each do |item|
+            @item = Item.new()
+            @item.service = "rss"
+            @item.content = item.description
+            @item.created_at = item.date.to_date
+            @item.user_id = account.user_id
+            @item.save() if Item.find_by_content(item.description).nil?
+          end
+end
       end
 
   end
